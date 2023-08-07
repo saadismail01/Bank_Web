8584f761e959e795ac4eace7aad63bbbf69b7504
Saad <saadismail001@gmail.com>
2023-08-07 23:53:14 +0300

    download assets and framework

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+import styles from "./style";
+styles;
+import {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Client,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full h-full">
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+            <h1>hello world</h1>
+          </div>
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Billing />
+          <Business />
+          <Button />
+          <CardDeal />
+          <Client />
+          <CTA />
+          <FeedbackCard />
+          <Footer />
+          <GetStarted />
+          <Stats />
+          <Testimonials />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
