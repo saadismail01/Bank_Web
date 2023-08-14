cf7e4996b600678008975d70158f0275bc912702
Saad <saadismail001@gmail.com>
2023-08-14 23:37:08 +0300

    Update: Left Side In Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,32 @@
-import React from "react";
-React;
+// eslint-disable-next-line no-unused-vars
+import styles from "../style";
+// eslint-disable-next-line no-unused-vars
+import { discount, robot } from "../assets";
 
-const Hero = () => {
-  return <div></div>;
-};
+const Hero = () => (
+  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div
+      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+    >
+      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+        <img src={discount} alt="discount" className="w-[32] h-[32px]" />
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className="text-white">20%</span> Discount For {""}
+          <span className="text-white">1 Month</span> Account
+        </p>
+      </div>
+
+      <div className=" flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+          The Next
+          <br className="sm:block hidden" /> {""}
+          <span className="text-gradient"> Generations</span> {""}
+          <br />
+          payment Method
+        </h1>
+      </div>
+    </div>
+  </section>
+);
 
 export default Hero;
