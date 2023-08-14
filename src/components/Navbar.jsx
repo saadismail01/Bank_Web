13d0d39f333b1b660bfc9cda6779eb56c4ea4cbf
Saad <saadismail001@gmail.com>
2023-08-14 23:36:20 +0300

    End Of Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ export const Navbar = () => {
           className={`${toggle ? `flex` : `hidden`}
           p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2  min-w-[140px] rounded-xl sidebar `}
         >
-          <ul className="list-none  flex flex-col justify-end items-center flex-1">
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
             {navLinks.map((nav) => (
               <li
                 key={nav.id}
