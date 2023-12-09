d89a8fd85de77118536b0eb034540566b0133e2b
Saad <saadismail001@gmail.com>
2023-12-09 22:56:06 +0200

    start generating the business content

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,7 +2,24 @@ import React from "react";
 React;
 
 const Business = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-between p-10">
+      <div className="w-[400px] space-y-6">
+        <h1 className="text-white text-3xl text-bold font-poppins">
+          You do the Business, we`ll handle the money.
+        </h1>
+        <p className=" text-gray-400 font-poppins">
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+        <button className="p-3 font-poppins text-black bg-blue-gradient rounded-md">
+          Get Started
+        </button>
+      </div>
+      <div className="text-white">hish</div>
+    </div>
+  );
 };
 
 export default Business;
