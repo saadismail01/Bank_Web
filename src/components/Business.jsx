ac86298124715c1269ea5e8fa192a8b78bc825d9
Saad <saadismail001@gmail.com>
2023-12-20 23:58:47 +0200

    adding the right side of business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,10 +1,39 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import styles, { layout } from "../style";
+import { features } from "../constants/index.js";
+import Button from "./Button";
 React;
+
+// eslint-disable-next-line react/prop-types
+const FeatureCard = ({ icon, title, content, index }) => {
+  return (
+    <div
+      className={`flex flex-row p-6 rounded-[20px] ${
+        index !== features.length - 1 ? "mb-6" : "mb-0"
+      } feature-card`}
+    >
+      <div
+        className={`w-[64px] h-[64px] rounded-gull ${styles.flexCenter} bg-dimBlue`}
+      >
+        <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+      </div>
+      <div className="flex-1 flex  flex-col ml-3">
+        <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+          {title}
+        </h4>
+        <p className="font-poppins font-normal text-white text-[16px] leading-[24px] mb-1">
+          {content}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 const Business = () => {
   return (
     <div className="flex justify-between p-10">
-      <div className="w-[400px] space-y-6">
+      <div className="max-w-[520px] space-y-20 pt-10 pb-10">
         <h1 className="text-white text-3xl text-bold font-poppins">
           You do the Business, we`ll handle the money.
         </h1>
@@ -13,11 +42,15 @@ const Business = () => {
           building credit, earning rewards and saving money. But with hundreds
           of credit cards on the market.
         </p>
-        <button className="p-3 font-poppins text-black bg-blue-gradient rounded-md">
+        <button className="p-3 font-poppins text-black bg-blue-gradient rounded-sm">
           Get Started
         </button>
       </div>
-      <div className="text-white">hish</div>
+      <div className={`${layout.sectionImg} flex-col max-w-[520px]`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
     </div>
   );
 };
