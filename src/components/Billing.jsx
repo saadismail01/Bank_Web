dc174c8d94f14470124f99f428230c040256117a
Saad <saadismail001@gmail.com>
2023-12-22 17:51:52 +0200

    generating of billing page done

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -6,15 +6,40 @@ import styles, { layout } from "../style";
 
 const Billing = () => {
   return (
-    <section id="product" className={layout.sectionImgReverse}>
-      <div className={layout.sectionReverse}>
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
         <img
           src={bill}
           alt="billing"
-          className="w-[100%] h-[100%] relative z-[5] sm:w-[50%] sm:h-[50%]"
+          className="w-[100%] h-[100%] relative z-[5]"
         />
         <div className="absolute z-[3] -left-1/2 top-0 w-[90%] h-[90%] rounded-full blue__gradient" />
         <div className="absolute z-[5] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink-gradient" />
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" /> billing &
+          invoicing
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img
+            src={apple}
+            alt="apple-store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="apple-store"
+            className="w-[128px] h-[42px] object-contain  cursor-pointer"
+          />
+        </div>
       </div>
     </section>
   );
