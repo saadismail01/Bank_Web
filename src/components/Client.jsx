ff9a9aa6515dd91ea05110b286ef0baec8cc88ce
Saad <saadismail001@gmail.com>
2023-12-26 14:59:15 +0200

    generate client section

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,8 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { clients } from "../constants";
+import styles from "../style";
 import React from "react";
 React;
 
 const Client = () => {
-  return <div></div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+          >
+            <img
+              src={client.logo}
+              alt="client"
+              className="sm:w-[192px] w-[100px] object contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Client;
